eb6dd2c28b892b554d0a82325c560be538bbb0dc
alfred <alfred@DESKTOP-86LIL25>
2025-02-13 17:01:15 +0700

    Answer to Extract Class

---
+++ src/exercise/ExtractClassAnswer.java
@@ -0,0 +1,43 @@
+package exercise;
+
+public class ExtractClassAnswer {
+// todo extract class TelephoneNumber from class Person
+	class Person {
+		private String name;
+		private TelephoneNumber telephoneNumber;
+
+		public String getName() {
+			return name;
+		}
+		
+		public String getTelephoneNumber() {
+			return telephoneNumber.getTelephoneNumber();
+		}
+
+	}
+
+	class TelephoneNumber {
+		private String officeAreaCode;
+		private String officeNumber;
+
+		public String getTelephoneNumber() {
+			return ("(" + officeAreaCode + ") " + officeNumber);
+		}
+
+		public String getOfficeAreaCode() {
+			return officeAreaCode;
+		}
+
+		public void setOfficeAreaCode(String arg) {
+			officeAreaCode = arg;
+		}
+
+		public String getOfficeNumber() {
+			return officeNumber;
+		}
+
+		public void setOfficeNumber(String arg) {
+			officeNumber = arg;
+		}
+	}
+}
