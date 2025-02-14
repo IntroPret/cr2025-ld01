df2ed90ab75234c5ccb5ebdd87945d23aeaa6dd1
alfred <alfred@DESKTOP-86LIL25>
2025-02-14 18:45:57 +0700

    Decompose Conditional Answer

---
+++ src/exercise/DecomposeConditionalAnswer.java
@@ -0,0 +1,56 @@
+package exercise;
+
+import java.util.Date;
+
+public class DecomposeConditionalAnswer {
+
+	class Stadium {
+		private final Date SUMMER_START = new Date(1500);
+		private final Date SUMMER_END = new Date(2500);
+		public double summerRate;
+		public double winterRate;
+		public double winterServiceCharge;
+
+		public Stadium(double summer, double winter, double service) {
+			this.summerRate = summer;
+			this.winterRate = winter;
+			this.winterServiceCharge = service;
+		}
+
+		// TODO: change the conditional logic in this method using
+		// decompose conditional
+		public boolean notSummer(Date date) {
+			return (date.before(SUMMER_START) || date.after(SUMMER_END)) ? true : false;
+		}
+
+		public double winterCharge(int quantity) {
+			return quantity * winterRate + winterServiceCharge;
+		}
+
+		public double summerCharge(int quantity) {
+			return quantity * summerRate;
+		}
+
+		public double getTicketPrice(Date date, int quantity) {
+			double charge;
+			if (notSummer(date)) {
+				charge = winterCharge(quantity);
+			} else {
+				charge = summerCharge(quantity);
+			}
+			return charge;
+		}
+	}
+
+	public void test() {
+		Stadium s = new Stadium(100, 90, 25);
+		System.out.println(s.getTicketPrice(new Date(1000), 10));
+		System.out.println(s.getTicketPrice(new Date(2000), 10));
+		System.out.println(s.getTicketPrice(new Date(3000), 10));
+	}
+
+	public static void main(String[] args) {
+		new DecomposeConditional().test();
+	}
+
+}
