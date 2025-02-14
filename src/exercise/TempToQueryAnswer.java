5dba78b3a73dfc3a425553f0c6658cb4b919a9c9
alfred <alfred@DESKTOP-86LIL25>
2025-02-14 23:08:12 +0700

    Temp To Query Answer

---
+++ src/exercise/TempToQueryAnswer.java
@@ -0,0 +1,40 @@
+package exercise;
+
+public class TempToQueryAnswer {
+	class Product {
+		private int quantity;
+		private int itemPrice;
+
+		public Product(int quantity, int price) {
+			this.quantity = quantity;
+			this.itemPrice = price;
+		}
+
+		// TODO: change this method to using replace temp with query
+		public double getPrice() {
+			double discountFactor;
+			if (getBasePrice() > 1000) {
+				discountFactor = 0.95;
+			} else {
+				discountFactor = 0.98;
+			}
+			return getBasePrice() * discountFactor;
+		}
+		
+		public int getBasePrice() {
+			return quantity * itemPrice;
+		}
+	}
+
+	public void Test() {
+		Product p1 = new Product(100, 200);
+		Product p2 = new Product(10, 50);
+		System.out.println("Price 1 = " + p1.getPrice());
+		System.out.println("Price 2 = " + p2.getPrice());
+	}
+
+	public static void main(String[] args) {
+		TempToQueryAnswer t = new TempToQueryAnswer();
+		t.Test();
+	}
+}
